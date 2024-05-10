d066776683f61fd4676263cbdaf732a4b20aca98
Padmasri Srinivas <114862496+Psri-01@users.noreply.github.com>
2024-05-11 00:26:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-const header = document.querySelector(".navbar")
+const navbar = document.querySelector(".navbar");
+const sections = document.querySelectorAll("section"); // Assuming all your sections have the class "section"
 
 window.onscroll = function() {
-    var top = window.scrollY;
-    if(top >=100) {
-        header.classList.add('navbarDark');
-    }
-    else {
-        header.classList.remove('navbarDark');
-    }
+  var top = window.scrollY;
+  if (top >= 100) {
+    navbar.classList.add('navbarLight');
+  } else {
+    navbar.classList.remove('navbarLight');
+  }
 }
+
+sections.forEach(section => {
+  section.addEventListener('click', () => {
+    const navbarCollapse = document.querySelector('.collapse');
+    // Use Bootstrap's collapse toggle method instead of directly manipulating classes
+    navbarCollapse.bsCollapse('hide'); 
+  });
+});
